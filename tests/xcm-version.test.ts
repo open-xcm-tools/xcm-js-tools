c5233348fb3bc44da084677cf51aeecef1cc3f90
Skvortsov A.V <alexskvr03@gmail.com>
2024-07-23 16:25:42 +0300

    added tests for converting from v4 to v3

--- tests/xcm-version.test.ts
+++ tests/xcm-version.test.ts
@@ -2,43 +2,50 @@ import { describe, expect, it } from "vitest";
 import { convertLocationVersion } from "../src/util.ts"
 import { VersionedLocation } from "../src/xcmtypes.ts";
 
+const locationV4: VersionedLocation = {V4: { parents: 1, interior: { X1: [{ GlobalConsensus: 'Kusama' }]}}};
+const locationWithBitcoinV4: VersionedLocation = {V4: { parents: 1, interior: { X1: [{ GlobalConsensus: 'BitcoinCore' }]}}};
+const locationWithMultipleJunctionsV4: VersionedLocation = {V4: { parents: 2, interior: {
+        X3: [{ GlobalConsensus: 'Polkadot' }, { Parachain: 200 }, { AccountId32: {network: 'BitcoinCore', id: 'TEST'}}]
+      }}};
+
+
+const locationV3: VersionedLocation = { V3: { parents: 1, interior: { X1: { GlobalConsensus: 'Kusama' } } }};
+const locationBitcoinV3: VersionedLocation = { V3: { parents: 1, interior: { X1: { GlobalConsensus: 'BitcoinCore' } } }};
+const locationMultipleJunctionsV3: VersionedLocation = {V3: { parents: 2, interior: { X3: [{ GlobalConsensus: 'Polkadot' }, { Parachain: 200 }, { AccountId32: {network: 'BitcoinCore', id: "TEST"}}] } }}
+
+
+describe('create versions from v4', () => {
+    it('create v4 from v4 location', () => {
+        expect(convertLocationVersion(4, locationV4.V4)).toStrictEqual(locationV4);
+        expect(convertLocationVersion(4, locationWithBitcoinV4.V4)).toStrictEqual(locationWithBitcoinV4);
+        expect(convertLocationVersion(4, locationWithMultipleJunctionsV4.V4)).toStrictEqual(locationWithMultipleJunctionsV4);
+    }) 
+
+    it('create v3 from v4 location', () => {
+        expect(convertLocationVersion(3, locationV4.V4)).toStrictEqual(locationV3);
+        expect(convertLocationVersion(3, locationWithBitcoinV4.V4)).toStrictEqual(locationBitcoinV3);
+        expect(convertLocationVersion(3, locationWithMultipleJunctionsV4.V4)).toStrictEqual(locationMultipleJunctionsV3);
+    })
+})
+
+
+
 describe('asset location convert v4->v3', () => {
     it('location v4->v3', () => {
-        const locationV4: VersionedLocation = {
-            V4: {
-              parents: 1,
-              interior: {
-                X1: [{ GlobalConsensus: 'Kusama' }]
-              }
-            }
-          };
-
-        expect(convertLocationVersion(3, locationV4)).toStrictEqual({ V3: { parents: 1, interior: { X1: { GlobalConsensus: 'Kusama' } } }});
+        expect(convertLocationVersion(3, locationV4)).toStrictEqual(locationV3);
     })
 
     it('location v4->v3 bitcoin', () => {
-        const locationWithBitcoin: VersionedLocation = {
-            V4: {
-              parents: 1,
-              interior: {
-                X1: [{ GlobalConsensus: 'BitcoinCore' }]
-              }
-            }
-          };
-
-        expect(convertLocationVersion(3, locationWithBitcoin)).toStrictEqual({ V3: { parents: 1, interior: { X1: { GlobalConsensus: 'BitcoinCore' } } }})
+        expect(convertLocationVersion(3, locationWithBitcoinV4)).toStrictEqual(locationBitcoinV3)
     })
 
     it('location v4->v3 with miltiple junction', () => {
-        const locationWithMultipleJunctions: VersionedLocation = {
-            V4: {
-              parents: 2,
-              interior: {
-                X2: [{ GlobalConsensus: 'Polkadot' }, { Parachain: 200 }]
-              }
-            }
-          };
-
-        expect(convertLocationVersion(3, locationWithMultipleJunctions)).toStrictEqual({V3: { parents: 2, interior: { X2: [{ GlobalConsensus: 'Polkadot' }, { Parachain: 200 }] } }})
+        expect(convertLocationVersion(3, locationWithMultipleJunctionsV4)).toStrictEqual(locationMultipleJunctionsV3)
     })
+})
+
+
+describe("asset location convert v3->v2", () => {
+
+    
 })

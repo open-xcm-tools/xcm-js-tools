3eb6b90cdaa608e28cddd4e717f1f1e33b764fbf
Daniel Shiposha <mrshiposha@gmail.com>
2024-08-05 17:26:31 +0200

    fix: add DryRunApi type decorations

--- src/simplexcm.ts
+++ src/simplexcm.ts
@@ -120,7 +120,71 @@ export class SimpleXcm {
     const chainInfo = registry.chainInfoById(chainId);
 
     const provider = new WsProvider([...chainInfo.endpoints]);
-    const api = await ApiPromise.create({ provider });
+    const api = await ApiPromise.create({
+      provider,
+      runtime: {
+        DryRunApi: [
+          {
+            version: 1,
+
+            methods: {
+              dry_run_call: {
+                description: 'XCM dry-run call',
+                params: [
+                  {
+                    name: 'origin',
+                    type: 'OriginCaller',
+                  },
+                  
+                  {
+                    name: 'call',
+                    type: 'Call',
+                  }
+                ],
+                type: 'Result<DryRunCallEffects, DryRunError>',
+              },
+
+              dry_run_xcm: {
+                description: 'XCM dry-run xcm',
+                params: [
+                  {
+                    name: 'origin_location',
+                    type: 'XcmVersionedLocation',
+                  },
+
+                  {
+                    name: 'xcm',
+                    type: 'VersionedXcm',
+                  }
+                ],
+                type: 'Result<DryRunXcmEffects, DryRunError>',
+              }
+            }
+          }
+        ]
+      },
+      types: {
+        DryRunCallEffects: {
+          execution_result: 'Result<FrameSupportDispatchPostDispatchInfo, SpRuntimeDispatchErrorWithPostInfo>',
+          emitted_events: 'Vec<Event>',
+          local_xcm: 'Option<XcmVersionedXcm>',
+          forwarded_xcms: 'Vec<(XcmVersionedLocation, Vec<XcmVersionedXcm>)>',
+        },
+
+        DryRunXcmEffects: {
+          execution_result: 'StagingXcmV4TraitsOutcome',
+          emitted_events: 'Vec<Event>',
+          forwarded_xcms: 'Vec<(XcmVersionedLocation, Vec<XcmVersionedXcm>)>',
+        },
+
+        DryRunError: {
+          _enum: {
+            Unimplemented: 'Null',
+            VersionedConversionFailed: 'Null',
+          },
+        },
+      },
+    });
 
     const palletXcm = await SimpleXcm.#findPalletXcm(api);
     if (!palletXcm) {

dd41543f2a1e5aed0322085c2ae08acbc3b8059f
Daniel Shiposha <mrshiposha@gmail.com>
2024-11-12 14:04:46 +0100

    chore: remove unneeded comments from up.sh

--- .baedeker/up.sh
+++ .baedeker/up.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -eux
 
-# docker pull parity/polkadot-parachain:1.14.0
-# docker pull uniquenetwork/builder-polkadot:release-v1.0.0
-
 if [ ! -e baedeker ]; then
     curl -L https://github.com/UniqueNetwork/baedeker/releases/download/v0.1.5/baedeker -o baedeker
     chmod +x baedeker
